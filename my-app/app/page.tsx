5d66647465aef8d853b2c7471de424c5f609a2b1
Honour0512 <63160822+sigbekuhonour@users.noreply.github.com>
2026-03-22 21:40:25 -0230

    feat: Integrate Vercel Analytics into the application.

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,6 +2,7 @@ import { Body } from "./body";
 import { Header } from "./header";
 import { Hero } from "./hero";
 import { Footer } from "./footer";
+import { Analytics } from "@vercel/analytics/next";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -10,6 +11,7 @@ export default function Home() {
         <Hero />
         <Body />
         <Footer />
+        <Analytics/>
       </main>
     </div>
   );
